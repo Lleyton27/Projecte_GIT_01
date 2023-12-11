0d9be4d484d4257a9fa1b26297bd1802473c51d9
Lleyton27 <lleyton.camram.dam@insalfonscostafreda.cat>
2023-12-11 18:36:17 +0100

    Primer cambio

---
+++ src/main/java/com/mycompany/projecte_git_01/Projecte_GIT_01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projecte_git_01;
+
+/**
+ *
+ * @author lleyton.camram.dam_i
+ */
+public class Projecte_GIT_01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
